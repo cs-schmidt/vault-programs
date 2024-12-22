765897912a9bd011cee879dbc62a252d8b606e39
cs-schmidt <cs-schmidt@users.noreply.github.com>
2024-12-22 02:53:31 -0700

    feat: complete leetcode problem 459

--- leetcode/leetcode-459.py
+++ leetcode/leetcode-459.py
@@ -6,11 +6,14 @@ Constraints:
     2. str contains only lowercase English letters.
 """
 
-# TODO: Complete solution.
+# TODO: Improve solution's time complexity.
+# NOTE: There's an alternative approach with O(1) auxiliary space and better time.
+
+from collections import deque
 
 
 class Solution:
-    def repeatedSubstringPattern(self, str: str) -> bool:
+    def repeatedSubstringPattern(self, s: str) -> bool:
         """
         Function: is_patterned_string
         Algorithmic Paradigm: Brute-Force Search
@@ -21,32 +24,27 @@ class Solution:
             - Time (Worst): O(n*sqrt(n)).
             - Space (Mean): O(sqrt(n)) auxiliary space.
         """
-        if len(str) <= 1: return False
-        bound = 1
-        width = 0
-        while bound < len(str):
-            # Search for the next possible candidate.
-            while bound < len(str) and str[bound] != str[0]: bound += 1
-            if bound > len(str) // 2: return False
-            width = self.distance(bound, 0)
-            if len(str) % width != 0:
-                bound += 1
-                continue
-            for i in range(width):
-                if str[bound] != str[i]: break
-                bound += 1
+        viable_sizes: deque[int] = deque(self.possible_pattern_sizes(s))
+        while viable_sizes:
+            size: int = viable_sizes.popleft()
+            head: int = size
+            while self.divides(size, head):
+                for i in range(size):
+                    if s[head] != s[i]: break
+                    head += 1
+                if head == len(s): return True
+                if not self.divides(size, head) or s[head] != s[0]: break
+        return False
 
-            # Handle whether or not a candidate was found.
-            if bound <= 2 * width - 1: continue
-            while bound < len(str):
-                steps = 0
-                for i in range(width):
-                    if str[bound] != str[i]: break
-                    bound += 1
-                    steps += 1
-                if steps < width: break
+    def possible_pattern_sizes(self, s: str) -> list[int]:
+        possible_sizes: list[int] = []
+        max_size_viable: int = len(s) // (2 if self.is_even(len(s)) else 3)
+        for i in range(1, max_size_viable + 1):
+            if s[i] == s[0] and self.divides(i, len(s)): possible_sizes.append(i)
+        return possible_sizes
 
-        return True
+    def is_even(self, num: int) -> bool:
+        return self.divides(2, num)
 
-    def distance(self, point1: int, point2: int):
-        return abs(point1 - point2)
+    def divides(self, num_1: int, num_2: int) -> bool:
+        return num_2 % num_1 == 0

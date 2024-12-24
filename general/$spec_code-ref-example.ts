4515b0b0417796ced6c7bf8b146fd972b1fc2333
cs-schmidt <cs-schmidt@users.noreply.github.com>
2024-12-23 19:33:33 -0700

    feat: update general files

--- general/$spec_code-ref-example.ts
+++
@@ -1,4 +0,0 @@
-/** Prints a greeting for `name`. */
-function greet(name: string): string {
-  console.log(`Hello ${name}`);
-}
---
+++ general/$spec_code-snippet.ts
@@ -0,0 +1,7 @@
+/**
+ * Logs a greeting message to the console.
+ * @param name - The name of the thing to greet.
+ */
+export function greet(name: string): void {
+  console.log(`Hello ${name}`);
+}
